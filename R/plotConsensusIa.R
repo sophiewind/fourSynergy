ed1a62898069452f8a941c60433e8f903a8c7341
swind <sophiewind@uni-muenster.de>
2025-11-06 15:07:31 +0000

    add highlights and spider

--- R/plotConsensusIa.R
+++ R/plotConsensusIa.R
@@ -10,6 +10,8 @@
 #' @param cex.ideo character expansion base numbers of ideogram.
 #' @param cex.y.track character expansion y axis track.
 #' @param cex.vp character expansion viewpoint label.
+#' @param highlight_regions regions to highlight in the plot
+#' @param plot_spider plotting connections from VP to interactions
 #'
 #' @return karyoplot with calling results.
 #'
@@ -29,7 +31,8 @@
 #' @export
 plotConsensusIa <- function(ia = GRangesList(), genes_of_interest = NULL,
                             cex.chr = 1, cex.ideo = 0.6, cex.y.track = 0.6,
-                            cex.vp = 1) {
+                            cex.vp = 1, highlight_regions = NULL, 
+                            plot_spider = FALSE) {
     TxDb <- switch(ia@metadata$organism,
         "mm9" = TxDb.Mmusculus.UCSC.mm9.knownGene,
         "mm10" = TxDb.Mmusculus.UCSC.mm10.knownGene,
@@ -43,56 +46,98 @@ plotConsensusIa <- function(ia = GRangesList(), genes_of_interest = NULL,
         kp <- createKaryoplot(ia, type = "2", cex = cex.chr)
     }
 
+    # Regions
+    if (!is.null(highlight_regions)){
+      for (i in seq(1, length(highlight_regions))){
+        kpBars(kp,
+               chr = paste0("chr", ia@metadata$VPchr), 
+               x0 = start(highlight_regions[[i]]),
+               x1 = end(highlight_regions[[i]]),
+               y0 = 0, y1 = 1,
+               r0 = 0.5, r1 = 1, col = adjustcolor("grey", alpha.f = 0.4),
+               border = NA
+        )
+      }
+    }
+    
     # Tracks
     bgs <- readBedGraph(ia)
     plotTracks(ia, kp, bgs,
-        r0 = 0.5, r1 = 1, cex.vp = cex.vp,
-        cex.y.track = cex.y.track
+               r0 = 0.5, r1 = 1, cex.vp = cex.vp,
+               cex.y.track = cex.y.track
     )
-
+    
+    
+    if(plot_spider == TRUE){
+      r_height <- c(0.4, 0.225, 0.175)
+      kpAbline(kp, chr = paste0("chr", ia@metadata$VPchr), h = r_height[2])
+      
+      start.reg <- GRanges(
+        seqnames = seqnames(ia@vp),
+        ranges = start(ia@vp)
+      )
+      start.regs <- rep(start.reg, length(ia@expConsensus[ia@expConsensus$significance > 0]))
+      kpPlotLinks(kp, start.regs, ia@expConsensus[ia@expConsensus$significance > 0],
+                  col = "firebrick4",r0 = r_height[1], r1 = 0.45)
+      start.regs_ctrl <- rep(start.reg, length(ia@ctrlConsensus[ia@ctrlConsensus$significance > 0]))
+      kpPlotLinks(kp, start.regs_ctrl, ia@ctrlConsensus[ia@ctrlConsensus$significance > 0],
+                  col = "darkblue",r0 = r_height[3], r1 = r_height[2])
+    } else {
+      r_height <- c(0.45, 0.225, 0.225)
+    }
+    # Structure plot
+    kpAbline(kp, chr = paste0("chr", ia@metadata$VPchr), h = r_height[1])
+    kpAbline(kp, chr = paste0("chr", ia@metadata$VPchr), h = r_height[3])
+    
     # Interactions
     if (length(ia@expConsensus) == 0) {
-        warning(
-            "fourSynergy found no interactions in condition. Did you run",
-            " `consensusIa()`?"
-        )
+      warning(
+        "fourSynergy found no interactions in condition. Did you run",
+        " `consensusIa()`?"
+      )
     } else if (length(ia@expConsensus[ia@expConsensus$significance > 0]) == 0) {
-        warning("fourSynergy found no interactions in condition.")
+      warning("fourSynergy found no interactions in condition.")
     } else {
-        kpPlotRegions(kp, ia@expConsensus[ia@expConsensus$significance > 0],
-            col = "firebrick4", r0 = 0.45, r1 = 0.225
-        )
+      kpPlotRegions(kp, ia@expConsensus[ia@expConsensus$significance > 0],
+                    col = "firebrick4", r0 = r_height[2], r1 = r_height[1]
+      )
+      kpAddLabels(kp,
+                  labels = "condition", r0 = r_height[2], r1 = r_height[1],
+                  cex = 0.7, data.panel = 1)
     }
-
+    
     if (length(ia@ctrlConsensus) == 0) {
-        warning(
-            "fourSynergy found no interactions in control. Did you run",
-            "`consensusIa()`?"
-        )
+      warning(
+        "fourSynergy found no interactions in control. Did you run",
+        "`consensusIa()`?"
+      )
     } else if (length(ia@ctrlConsensus[ia@ctrlConsensus$significance > 0]) ==
-        0) {
-        warning("fourSynergy found no interactions in control.")
+               0) {
+      warning("fourSynergy found no interactions in control.")
     } else {
-        kpPlotRegions(kp, ia@ctrlConsensus[ia@ctrlConsensus$significance > 0],
-            col = "darkblue", r0 = 0, r1 = 0.225
-        )
+      kpPlotRegions(kp, ia@ctrlConsensus[ia@ctrlConsensus$significance > 0],
+                    col = "darkblue", r0 = 0, r1 = r_height[3]
+      )
+      kpAddLabels(kp,
+                  labels = "control", r0 = 0, r1 = r_height[3],
+                  cex = 0.7, data.panel = 1)
     }
-
+    
     # Genes
     if (!is.null(genes_of_interest)) {
-        plot_genes(ia, kp, genes_of_interest, TxDb)
+      plot_genes(ia, kp, genes_of_interest, TxDb)
     }
-
+    
     # VP
     kpAbline(kp, v = start(ia@vp), col = "black", lty = 2, data.panel = 1)
     kpAddBaseNumbers(kp,
-        tick.dist = 100000, tick.len = 10, cex = cex.ideo,
-        minor.tick.col = "gray"
+                     tick.dist = 100000, tick.len = 10, cex = cex.ideo,
+                     minor.tick.col = "gray"
     )
-
+    
     legend(0.85, 0.8,
-        legend = c("condition", "control"),
-        fill = c("firebrick4", "darkblue"), border = NA,
-        bty = "o", cex = 0.6
+           legend = c("condition", "control"),
+           fill = c("firebrick4", "darkblue"), border = NA,
+           bty = "o", cex = 0.6
     )
 }
